6059f32689c4248006dfa72b5b59ef8859029a30
doray <mewoabid@gmail.com>
2024-02-05 00:23:01 +0100

    created dev branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react' 
 import './App.css'
 import DataTable from './components/DataTable'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <DataTable/>
+        <h1>My Custom React DataTable </h1>
+        <DataTable />
       </div>
     </>
   )
--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DataTable = () => {
   return (
     <div>
-        <h1>My Custom React DataTable </h1>
+       
     </div>
   )
 }
