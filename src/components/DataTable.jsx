285d01aab5ffa673621288b90c158676634fdc47
doray <mewoabid@gmail.com>
2024-02-05 15:10:19 +0100

    adding new rows of data to the table

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -7,6 +7,15 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import './table.css'
 
 const iconStyles = {
@@ -17,10 +26,28 @@ const iconStyles = {
   marginRight: "2px"
 }
 
+//styles for the modal
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 const DataTable = () => {
+  //states
   const [students, setStudents] = useState([]);
-  const [studentToAdd, setStudentToAdd] = useState({});
+  const [studentToAdd, setStudentToAdd] = useState({ name: "", matricule: "", level: 1, gender: "male", age: 15 });
+  const [open, setOpen] = useState('false');
+
+  const handleOpen = (value) => setOpen(value);
+  const handleClose = () => setOpen('false');
 
   useEffect(() => {
     const fetchStudents = async () => {
@@ -35,9 +62,33 @@ const DataTable = () => {
     fetchStudents()
   }, [])
 
+  //crud operations 
+  const handleAddRow = async () => {
+    await fetch('http://localhost:8000/students', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: studentToAdd.name,
+        matricule: studentToAdd.matricule,
+        age: studentToAdd.age,
+        level: studentToAdd.level,
+        gender: studentToAdd.gender,
+      })
+    }).then(response => {
+      return response.json()
+    }).then(data => {
+      console.log(data)
+      alert("student added")
+      location.reload()
+    }).catch(err => {
+      console.log(err)
+    }) 
+  }
 
+
+  //custom components
   const AddStudent = (
-    <span onClick={()=>{handleOpen("addModal")}}>
+    <span onClick={() => { handleOpen("AddModal") }}>
       <PlaylistAddIcon />
     </span>
   )
@@ -53,6 +104,7 @@ const DataTable = () => {
     <span><FilterListIcon /></span>
   )
 
+
   const columns = [
     { key: "id", component: "Id" },
     { key: "name", component: "Name" },
@@ -62,7 +114,7 @@ const DataTable = () => {
     { key: "level", component: "Level" },
     { key: "addIcon", component: AddStudent }
   ]
-
+  //table functions 
   const createBodyTr = (student) => {
     return (
       <tr key={student.id} data-id={student.id}>
@@ -117,6 +169,113 @@ const DataTable = () => {
           </tr>
         </tbody>
       </table>
+
+
+      {/* {the AddModal goes here} */}
+      <Modal
+        open={open === "AddModal"}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-name"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-name" variant="h6" component="h2">
+            Fill the form to add a new student
+          </Typography>
+          <form noValidate autoComplete='off'>
+            <TextField label="name" value={studentToAdd.name} variant='standard' sx={{ marginBottom: "10px", display: "block" }}
+              onChange={(e) => {
+                setStudentToAdd({ 
+                  name: e.target.value,
+                  matricule: studentToAdd.matricule,
+                  age: studentToAdd.age,
+                  level: studentToAdd.level,
+                  gender: studentToAdd.gender
+                })
+              }}
+            />
+
+            <TextField label="matricule" value={studentToAdd?.matricule}
+              variant='standard' sx={{ marginBottom: "10px", display: "block" }}
+              onChange={(e) => {
+                setStudentToAdd({ 
+                  name: studentToAdd.name,
+                  matricule: e.target.value,
+                  age: studentToAdd.age,
+                  level: studentToAdd.level,
+                  gender: studentToAdd.gender
+                })
+              }}
+            />
+
+            <TextField label="age" value={studentToAdd?.age}
+              variant='standard' type='number' sx={{ marginBottom: "10px", display: "block" }}
+              onChange={(e) => {
+                setStudentToAdd({ 
+                  name: studentToAdd.name,
+                  matricule: studentToAdd.matricule,
+                  age: parseInt(e.target.value),
+                  level: studentToAdd.level,
+                  gender: studentToAdd.gender
+                })
+              }}
+            />
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, display: "block", marginBottom: "30px" }}>
+              <InputLabel id="demo-simple-select-standard-label">Level</InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={studentToAdd.level}
+                onChange={(e) => {
+                  setStudentToAdd({ 
+                    name: studentToAdd.name,
+                    matricule: studentToAdd.matricule,
+                    age: studentToAdd.age,
+                    level: e.target.value,
+                    gender: studentToAdd.gender,
+                  })
+                }}
+                label="gender"
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, display: "block", marginBottom: "30px" }}>
+              <InputLabel id="demo-simple-select-standard-label">Gender</InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={studentToAdd.gender}
+                onChange={(e) => {
+                  setStudentToAdd({ 
+                    name: studentToAdd.name,
+                    matricule: studentToAdd.matricule,
+                    age: studentToAdd.age,
+                    level: studentToAdd.level,
+                    gender: e.target.value,
+                  })
+                }}
+                label="gender"
+              >
+                <MenuItem value={"male"}>Male</MenuItem>
+                <MenuItem value={"female"}>Female</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Box sx={{ textAlign: "center", alignItems: "center" }}>
+              <Button onClick={() => {
+                handleAddRow()
+              }} variant='contained' sx={{ marginRight: "10%" }}> Addstudent </Button>
+              <Button variant='contained' onClick={() => { handleClose() }}>Cancel</Button>
+            </Box>
+          </form>
+        </Box>
+      </Modal>
     </div>
   )
 }
