cc601b628782d3036bb6b4eb6f9e16e9fa5e7072
doray <mewoabid@gmail.com>
2024-02-05 18:59:17 +0100

    editing and deleting of row records

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -17,6 +17,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import './table.css'
+import { Block } from '@mui/icons-material';
 
 const iconStyles = {
   borderRadius: "50%",
@@ -44,6 +45,8 @@ const DataTable = () => {
   //states
   const [students, setStudents] = useState([]);
   const [studentToAdd, setStudentToAdd] = useState({ name: "", matricule: "", level: 1, gender: "male", age: 15 });
+  const [studentToEdit, setStudentToEdit] = useState({id:0, name: "", matricule: "", level: 1, gender: "male", age: 15 });
+  const [currentID, setCurrentId] = useState(0);
   const [open, setOpen] = useState('false');
 
   const handleOpen = (value) => setOpen(value);
@@ -62,7 +65,57 @@ const DataTable = () => {
     fetchStudents()
   }, [])
 
-  //crud operations 
+
+  //function to delete and to edit a row of data 
+  const onDeleteClick = (id) => {
+    setCurrentId(id);
+    console.log("deleting row with id: ", id)
+    handleOpen("DeleteModal");
+  }
+
+  const onEditClick = (id) => {
+    console.log(studentToEdit)
+    setCurrentId(id);
+    handleOpen("EditModal");
+  }
+
+  const handleEditRow = async (id) => {
+    await fetch('http://localhost:8000/students/' + id, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: studentToEdit.name,
+        matricule: studentToEdit.matricule,
+        age: studentToEdit.age,
+        dateOfBirth: studentToEdit.dateOfBirth,
+        level: studentToEdit.level,
+        gender: studentToEdit.gender,
+      })
+    }).then(response => {
+      return response.json()
+    }).then(data => {
+      console.log(data)
+      alert("successsful")
+      location.reload()
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  const handleDeleteRow = async (id) => {
+    const response = await fetch('http://localhost:8000/students/' + id, {
+      method: "DELETE"
+    });
+    if (response.ok) {
+      alert("student deleted");
+      handleClose(); //close the modal 
+      location.reload()
+    } else {
+      alert("could not delete student")
+      handleClose()
+    }
+  }
+  //adding a row
   const handleAddRow = async () => {
     await fetch('http://localhost:8000/students', {
       method: 'POST',
@@ -82,7 +135,7 @@ const DataTable = () => {
       location.reload()
     }).catch(err => {
       console.log(err)
-    }) 
+    })
   }
 
 
@@ -115,6 +168,41 @@ const DataTable = () => {
     { key: "addIcon", component: AddStudent }
   ]
   //table functions 
+
+  const iconClicked = (e) => {
+    const elmtId = (e.target);
+    let parentId;
+    let callback;
+
+    if (elmtId.tagName === 'path') {
+      parentId = elmtId.parentElement?.parentElement?.parentElement?.getAttribute("data-id")
+      if (elmtId.parentElement?.getAttribute("data-testid") === "DeleteOutlineIcon") {
+        callback = onDeleteClick
+      } else {
+        callback = onEditClick
+      }
+    } else if (elmtId.tagName === 'svg') {
+      parentId = elmtId.parentElement?.parentElement?.getAttribute("data-id")
+      if (elmtId.getAttribute("data-testid") === "DeleteOutlineIcon") {
+        callback = onDeleteClick
+      } else {
+        callback = onEditClick
+      }
+    }
+
+    if (parentId && callback) {
+      const id = parentId; 
+      console.log("this is the id", id)
+      let foundStudent
+      students.forEach(student => {
+        if (student.id == id) {
+         foundStudent = student
+        }
+      }) 
+      setStudentToEdit(foundStudent ? foundStudent : studentToEdit) 
+      callback(id)
+    }
+  }
   const createBodyTr = (student) => {
     return (
       <tr key={student.id} data-id={student.id}>
@@ -124,7 +212,10 @@ const DataTable = () => {
         <td><span>{student.age}</span></td>
         <td><span>{student.gender}</span></td>
         <td><span>{student.level}</span></td>
-        <td className='icon-td' >
+        <td className='icon-td' onClick={(e) => {
+          iconClicked(e)
+        }
+        }>
           {<EditIcon sx={iconStyles} />}
           {<DeleteOutlineIcon sx={iconStyles} />}
         </td>
@@ -185,7 +276,7 @@ const DataTable = () => {
           <form noValidate autoComplete='off'>
             <TextField label="name" value={studentToAdd.name} variant='standard' sx={{ marginBottom: "10px", display: "block" }}
               onChange={(e) => {
-                setStudentToAdd({ 
+                setStudentToAdd({
                   name: e.target.value,
                   matricule: studentToAdd.matricule,
                   age: studentToAdd.age,
@@ -198,7 +289,7 @@ const DataTable = () => {
             <TextField label="matricule" value={studentToAdd?.matricule}
               variant='standard' sx={{ marginBottom: "10px", display: "block" }}
               onChange={(e) => {
-                setStudentToAdd({ 
+                setStudentToAdd({
                   name: studentToAdd.name,
                   matricule: e.target.value,
                   age: studentToAdd.age,
@@ -211,7 +302,7 @@ const DataTable = () => {
             <TextField label="age" value={studentToAdd?.age}
               variant='standard' type='number' sx={{ marginBottom: "10px", display: "block" }}
               onChange={(e) => {
-                setStudentToAdd({ 
+                setStudentToAdd({
                   name: studentToAdd.name,
                   matricule: studentToAdd.matricule,
                   age: parseInt(e.target.value),
@@ -228,7 +319,7 @@ const DataTable = () => {
                 id="demo-simple-select-standard"
                 value={studentToAdd.level}
                 onChange={(e) => {
-                  setStudentToAdd({ 
+                  setStudentToAdd({
                     name: studentToAdd.name,
                     matricule: studentToAdd.matricule,
                     age: studentToAdd.age,
@@ -252,7 +343,7 @@ const DataTable = () => {
                 id="demo-simple-select-standard"
                 value={studentToAdd.gender}
                 onChange={(e) => {
-                  setStudentToAdd({ 
+                  setStudentToAdd({
                     name: studentToAdd.name,
                     matricule: studentToAdd.matricule,
                     age: studentToAdd.age,
@@ -276,6 +367,143 @@ const DataTable = () => {
           </form>
         </Box>
       </Modal>
+
+      {/* {the EditModal goes here} */}
+      <Modal
+        open={open === "EditModal"}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-name"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-name" variant="h6" component="h2">
+            Fill the form to edit the student record {currentID}
+          </Typography>
+
+          <form noValidate autoComplete='off'>
+            <TextField label="name" value={studentToEdit?.name}
+              variant='standard' sx={{ marginBottom: "10px", display:"block" }}
+              onChange={(e) => {
+                setStudentToEdit({
+                  name: e.target.value,
+                  matricule: studentToEdit.matricule,
+                  age: studentToEdit.age,
+                  level: studentToEdit.level,
+                  gender: studentToEdit.gender
+                })
+              }}
+            />
+
+            <TextField label="matricule" value={studentToEdit?.matricule}
+              variant='standard' sx={{ marginBottom: "10px", display: "block" }}
+              onChange={(e) => {
+                setStudentToEdit({
+                  name: studentToEdit.name,
+                  matricule: e.target.value,
+                  age: studentToEdit.age,
+                  level: studentToEdit.level,
+                  gender: studentToEdit.gender
+                })
+              }}
+            />
+
+            <TextField label="age" value={studentToEdit?.age}
+              variant='standard' type='number' sx={{ marginBottom: "10px", display:"block" }}
+              onChange={(e) => {
+                setStudentToEdit({
+                  name: studentToEdit.name,
+                  matricule: studentToEdit.matricule,
+                  age: e.target.value,
+                  level: studentToEdit.level,
+                  gender: studentToEdit.gender,
+                  dateOfBirth: studentToEdit.dateOfBirth
+                })
+              }}
+            />
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, display: "block", marginBottom: "30px" }}>
+              <InputLabel id="demo-simple-select-standard-label">Level</InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={studentToAdd.level}
+                onChange={(e) => {
+                  setStudentToAdd({
+                    name: studentToEdit.name,
+                    matricule: studentToEdit.matricule,
+                    age: studentToEdit.age,
+                    level: e.target.value,
+                    gender: studentToEdit.gender,
+                  })
+                }}
+                label="gender"
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, display: "block", marginBottom: "30px" }}>
+              <InputLabel id="demo-simple-select-standard-label">Gender</InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={studentToEdit.gender}
+                onChange={(e) => {
+                  setStudentToEdit({
+                    name: studentToEdit.name,
+                    matricule: studentToEdit.matricule,
+                    age: studentToEdit.age,
+                    level: studentToEdit.level,
+                    gender: e.target.value
+                  })
+                }}
+                label="Age"
+              >
+                <MenuItem value={"male"}>male</MenuItem>
+                <MenuItem value={"female"}>female</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Box sx={{ textAlign: "center", alignItems: "center" }}>
+              <Button onClick={() => {
+                handleEditRow(currentID)
+              }} variant='contained' sx={{ marginRight: "10%" }}> save changes </Button>
+              <Button variant='contained' onClick={() => { handleClose() }}>Cancel</Button>
+            </Box>
+          </form>
+
+        </Box>
+      </Modal>
+      {/* {the DeleteModal goes here} */}
+      <Modal
+        open={open === "DeleteModal"}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-name"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-name" variant="h6" component="h2" gutterBottom>
+            Are you sure you want to remove this student
+          </Typography>
+          <Box sx={{ textAlign: "center" }}>
+            <Button variant='contained'
+              sx={{ marginRight: "30%" }}
+              onClick={() => {
+                handleDeleteRow(currentID)
+              }}
+            > Yes </Button>
+            <Button variant='contained'
+              onClick={() => {
+                handleClose();
+              }}
+            > no </Button>
+          </Box>
+        </Box>
+      </Modal>
+
     </div>
   )
 }
